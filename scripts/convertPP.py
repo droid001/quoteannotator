b51d6c2704d23fc187ad7090dc48abe9f8087976
Angel Chang <angelx@gmail.com>
2016-04-04 16:19:35 -0700

    Processing pride and prejudice.

--- scripts/convertPP.py
+++ scripts/convertPP.py
@@ -6,6 +6,7 @@ import argparse
 import collections
 import xml
 import os
+import re
 import sys
 import logging
 import traceback
@@ -22,12 +23,21 @@ def textToChapters(lines):
     for line in lines:
         if line.startswith('CHAPTER'):
             if len(current) > 0:
-                chapters.append(current)
+                chapters.append( { 'text': current } )
                 current = []
-            current.append(line)
+        current.append(line)
     if len(current) > 0:
-        chapters.append(current)
+        chapters.append({ 'text': current })
     return chapters
+
+def readlines(input):
+    lines = []
+    with open(input) as x:
+        for line in x:
+            line = line.strip()
+            if len(line):
+                lines.append(line)
+    return lines
 
 def convert(input, output):
     print input
@@ -36,14 +46,76 @@ def convert(input, output):
     textfile = os.path.join(input, 'PRIDPREJ_NONEWLINE_Organize_v2.txt')
     annfile = os.path.join(input, 'REAL_ALL_CONTENTS_PP.txt')
     # slurp in our inputs
-    with open(charfile) as x: characters = x.readlines()
-    with open(textfile) as x: text = x.readlines()
-    with open(annfile) as x: annotations = x.readlines()
+    #with open(charfile) as x: characters = x.readlines()
+    #with open(textfile) as x: text = x.readlines()
+    #with open(annfile) as x: annotations = x.readlines()
+    characters = readlines(charfile)
+    text = readlines(textfile)
+    annotations = readlines(annfile)
     print '# characters: ' + str(len(characters))
     print '# quotes: ' + str(len(annotations))
     print '# lines: ' + str(len(text))
     chapters = textToChapters(text)
     print '# chapters: ' + str(len(chapters))
+    # Initialize chapters for processing
+    #quote_pattern = re.compile('``.*''')
+    for chapter in chapters:
+        #print len(chapter['text'])
+        # Look for multiline quotes
+        inQuotes = False
+        quoted = []
+        startIndex = -1
+        multilineQuotes = []
+        for li, line in enumerate(chapter['text']):
+            qsi = line.find("``")
+            qei = line.find("''")
+            if (qsi >= 0 and qei < 0):
+                inQuotes = True
+                startIndex = li
+                quoted.append(line)
+            elif (qei >= 0 and qsi < 0):
+                inQuotes = False
+                multilineQuotes.append( {
+                    'quote': " ".join(quoted),
+                    'quoteLines': quoted,
+                    'span': [startIndex, li+1]
+                })
+                startIndex = -1
+                quoted = []
+            elif (qei < 0 and qsi < 0):
+                if inQuotes:
+                    quoted.append(line)
+        # Set index
+        print multilineQuotes
+        chapter['mulitlineQuotes'] = multilineQuotes
+        chapter['_nextIndex'] = 0
+    # Try to match annotations with the text
+    for ai, annstr in enumerate(annotations):
+        ann = annstr.split('\t')
+        ch = int(ann[0])
+        speaker = ann[1]
+        quote = ann[2]
+        chapter = chapters[ch-1]
+        startIndex = chapter['_nextIndex'];
+        pieces = re.split('\s*\[X\]\s*', quote)
+        escaped = map(re.escape, pieces)
+        regex = '(' + '.*'.join(escaped) + ')'
+        pattern = re.compile(regex)
+        matched = False
+        for i in range(startIndex, len(chapter['text'])):
+            # Try to match quote to text
+            line = chapter['text'][i]
+            #print regex
+            #print line
+            m = pattern.search(line)
+            if m:
+                print 'matched %d to chapter %d:%d' % (ai, ch, i)
+                matched = True
+                chapter['_nextIndex'] = i+1
+                break
+        if not matched:
+            print 'unmatched ' + annstr
+
 
 
 def main():
