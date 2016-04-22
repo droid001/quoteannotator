912e62cc240f24e5ca6ead56288ae00c75c32552
Angel Chang <angelx@gmail.com>
2016-04-22 11:24:28 -0700

    Map character to entities.

--- scripts/convertCQSC.py
+++ scripts/convertCQSC.py
@@ -11,14 +11,39 @@ import traceback
 import xml.dom.minidom as minidom
 
 from sets import Set
+from collections import Counter
 
 FORMAT = '%(asctime)-15s [%(levelname)s] %(message)s'
 logging.basicConfig(format=FORMAT)
 log = logging.getLogger('index')
 log.setLevel(logging.INFO)
 
+def strToCharacter(str):
+    fields = str.split(';')
+    aliases = [fields[0]] + fields[2:]
+    return { 'name': fields[0].replace(' ', '_'), 'gender': mapGender(fields[1]), 'aliases': aliases}
+
 def mapGender(gender):
-    return gender
+    if gender == 'M':
+        return 'male'
+    elif gender == 'F':
+        return 'female'
+    else:
+        return gender
+
+def readlines(input):
+    lines = []
+    with open(input) as x:
+        for line in x:
+            line = line.strip()
+            if len(line):
+                lines.append(line)
+    return lines
+
+def readCharacters(filename):
+    characters = readlines(filename)
+    characters = map(strToCharacter, characters)
+    return characters
 
 def get_all_text( node ):
     if node.nodeType ==  node.TEXT_NODE:
@@ -74,6 +99,10 @@ def writeXml( dom, filename, includeSectionTags ):
         output.write(dom.toxml("utf-8"))
 #       output.write(dom.toprettyxml(encoding="utf-8"))
 
+def writeEntities(dom, filename):
+    with open(filename, 'w') as output:
+       output.write(dom.toprettyxml(encoding="utf-8"))
+
 def writeConverted( dom, filename, splitChapters, includeSectionTags):
     if splitChapters:
         # Create minidom for each chapter
@@ -94,13 +123,34 @@ def writeConverted( dom, filename, splitChapters, includeSectionTags):
     else:
         writeXml(dom, filename, includeSectionTags)
 
-def convert(input, outfilename, mentionLevel, splitChapters, includeSectionTags):
+def findCharacter(entity, characters):
+    # Tries to match entity to list of characters
+    cnt = Counter()
+    for index,character in enumerate(characters):
+        for alias1 in entity['aliases']:
+            for alias2 in character['aliases']:
+                if alias2 == alias1:
+                    cnt[index] += 1
+    best = cnt.most_common(1)
+    if len(best) > 0:
+        character = characters[best[0][0]]
+        return character
+    else:
+        return None
+
+def convert(input, outfilename, charactersFile, mentionLevel, splitChapters, includeSectionTags):
     #print input
     #print output
     nertypes = ['PERSON', 'ORGANIZATION', 'LOCATION']
     dom = minidom.parse(input)
     root = dom.documentElement
-    # Process paragraphs    
+    if charactersFile:
+        characters = readCharacters(charactersFile)
+        characterDict = { x['name']:x for x in characters }
+    else:
+        characters = None
+        characterDict = None
+    # Process paragraphs
     entities = {}
     mentionIdToEntityId = {}
     # Clean extracted mentions in HEADING
@@ -136,6 +186,11 @@ def convert(input, outfilename, mentionLevel, splitChapters, includeSectionTags)
         'ORGANIZATION': { 'elements': dom.createElement('ORGANIZATIONS'), 'name': 'ORGANIZATION'},
     }
     for entityId, entity in entities.iteritems():
+        # try to match entity with our list of characters
+        if characters:
+            character = findCharacter(entity, characters)
+            if character:
+                entity['name'] = character['name']
         info = entityElementsByType[entity['entityType']]
         element = dom.createElement(info['name'])
         for k, v in entity.iteritems():
@@ -156,6 +211,19 @@ def convert(input, outfilename, mentionLevel, splitChapters, includeSectionTags)
     newdoc.appendChild(root)
     dom.appendChild(newdoc);
 
+    # Add characters
+    if characters:
+        charactersElement = dom.createElement('CHARACTERS')
+        for character in characters:
+            element = dom.createElement('CHARACTER')
+            charactersElement.appendChild(element)
+            for k, v in character.iteritems():
+                if k == 'aliases':
+                    element.setAttribute(k,';'.join(v))
+                else:
+                    element.setAttribute(k,v)
+        newdoc.insertBefore(charactersElement, entitiesElement)
+
     # Go over quotes and match them to characters
     quotes = dom.getElementsByTagName('QUOTE')
     speakerMentions = Set()
@@ -166,8 +234,11 @@ def convert(input, outfilename, mentionLevel, splitChapters, includeSectionTags)
         if speakerMentionId and speakerMentionId != 'none':
             speakerMentions.add(speakerMentionId)
             speakerId = mentionIdToEntityId[speakerMentionId]
+            entity = entities[speakerId]
+            speakerName =  entity['name'] if 'name' in entity else speakerId
             # Rename attributes
-            quote.setAttribute('speaker', speakerId)
+            quote.setAttribute('speakerId', speakerId)
+            quote.setAttribute('speaker', speakerName)
             quote.setAttribute('mention', speakerMentionId)
         else:
             noSpeaker += 1
@@ -193,11 +264,16 @@ def convert(input, outfilename, mentionLevel, splitChapters, includeSectionTags)
 
     # Output
     writeConverted(dom, outfilename, splitChapters, includeSectionTags)
+    
+    (base, ext) = os.path.splitext(outfilename)
+    writeEntities(entitiesElement, base + ".entities" + ext)
 
 def main():
     # Argument processing
     parser = argparse.ArgumentParser(description='Convert CQSC XML')
+    parser.add_argument('-c', '--characters', dest='charactersFile', help='characters file', action='store')
     parser.add_argument('-s', '--split', dest='splitChapters', help='split by chapter', action='store_true')
+    #parser.add_argument('--connection', dest='useConnection', help='use connection attribute', action='store_true')
     parser.add_argument('-p', dest='includeSectionTags', help='paragraphs and headings', action='store_true')
     parser.add_argument('infile', nargs='?', type=argparse.FileType('r'),
                         default=sys.stdin)
@@ -210,6 +286,7 @@ def main():
     for mentionLevel in mentionLevels:
         args.infile.seek(0)
         outfilename = outbase + '.' + mentionLevel.lower() + outext
-        convert(args.infile, outfilename, mentionLevel, args.splitChapters, args.includeSectionTags)
+        convert(args.infile, outfilename, args.charactersFile,
+            mentionLevel, args.splitChapters, args.includeSectionTags)
 
 if __name__ == "__main__": main()
