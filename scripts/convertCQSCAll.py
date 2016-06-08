bd6114ecddbf659009a4ab0d560b5bb87e0f34d5
Angel Chang <angelx@gmail.com>
2016-06-07 22:41:50 -0700

    Add option for marking nested quotes.

--- scripts/convertCQSCAll.py
+++ scripts/convertCQSCAll.py
@@ -21,6 +21,7 @@ def main():
     parser = argparse.ArgumentParser(description='Convert CQSC XML')
     parser.add_argument('-s', '--split', dest='splitChapters', help='split by chapter', action='store_true')
     parser.add_argument('-p', dest='includeSectionTags', help='paragraphs and headings', action='store_true')
+    parser.add_argument('-n', dest='extractNestedQuotes', help='exract nested quotes', action='store_true')
     parser.add_argument('indir', help='directory to use', action='store')
     args = parser.parse_args()
 
@@ -29,7 +30,7 @@ def main():
         filename = args.indir + "/" + file
         (base,ext) = os.path.splitext(filename)
         match = re.match(r"(.*)_(\d+)", base)
-        if match: 
+        if match:
             base = match.group(1)
         charactersFile = base + ".characters.json"
         if not os.path.isfile(charactersFile):
@@ -37,7 +38,7 @@ def main():
         print "convert " + filename + " with characters " + str(charactersFile)
         if args.splitChapters:
             base2 = os.path.basename(base)
-            outbase = args.indir + "/converted_split/" + base2 
+            outbase = args.indir + "/converted_split/" + base2
             outfile = outbase + "/" + file
         else:
             outbase = args.indir + "/converted/"
@@ -45,7 +46,7 @@ def main():
         if not os.path.exists(outbase):
             os.makedirs(outbase)
         try:
-            cqsc.convertMentionLevels(filename, outfile, charactersFile, args.splitChapters, args.includeSectionTags)
+            cqsc.convertMentionLevels(filename, outfile, charactersFile, args.splitChapters, args.includeSectionTags, args.extractNestedQuotes)
         except:
             log.error("Unexpected error processing " + filename + ": ", exc_info=True)
 
